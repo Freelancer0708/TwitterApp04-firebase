efe49d609293088ac22dc428cc3a65d206c66806
Freelancer0708 <freelancernoaccont@gmail.com>
2024-07-21 21:39:17 +0900

    update comment

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -6,5 +6,3 @@ export const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
-
-//下記のように記載しているのはダメですか？
